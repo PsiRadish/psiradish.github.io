f68730abd5c648ce68571b6165744aa46103aba5
PsiRadish <psiradish@comcast.net>
2015-11-09 16:56:36 -0800

    Hopefully fixed "portal" box behavior in Chrome

--- js/arcane.js
+++ js/arcane.js
@@ -1,21 +1,23 @@
 
+//// Weird things happen with portals when height not explicitly defined
+function arcaneResize()
+{
+    $('.portal').each(function()
+    {
+        var $this = $(this);
+        
+        $this.removeAttr('style');
+        console.log("Portal height", $this.outerHeight(false));
+        $this.height($this.outerHeight(false));
+    });
+}
+
 $(document).ready(function()
 {
-    //// Weird things happen with portals when height not explicitly defined
-    function size()
-    {
-        $('.portal').each(function()
-        {
-            var $this = $(this);
-            
-            $this.removeAttr('style');
-            $this.height($this.outerHeight(false));
-        });
-    }
-    // call it all the time
-    $(window).on('resize', size);
-    // and right now
-    size();
+    //// Do resize things on resize
+    $(window).on('resize', arcaneResize);
+    // and when the page finishes loading
+    $('body')[0].onload = arcaneResize;
     
     
     //// Immediately remove focus from clicked links and buttons
